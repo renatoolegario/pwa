96bf1c3b3b5d3414dfbfd68fe7a384a1fb1e7215
Renato Olegário <73908159+renatoolegario@users.noreply.github.com>
2024-03-05 18:04:26 +0000

    adicionado pwa

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,5 @@
-// _app.js
-import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    if ('serviceWorker' in navigator) {
-      window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js').then(registration => {
-          console.log('Service Worker registrado com sucesso:', registration.scope);
-        }).catch(error => {
-          console.error('Falha ao registrar o Service Worker:', error);
-        });
-      });
-    }
-  }, []);
 
   return <Component {...pageProps} />;
 }
