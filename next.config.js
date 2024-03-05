4d6383ba3a3e32af5ac552fe1b26ddb4f06d0c62
Renato Olegário <73908159+renatoolegario@users.noreply.github.com>
2024-03-05 19:15:42 +0000

    uai

---
+++ next.config.js
@@ -0,0 +1,9 @@
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+});
+
+module.exports = withPWA({
+  // Your Next.js configuration options
+});
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
+import React from 'react';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-
-  return <Component {...pageProps} />;
+  return (
+    <React.Fragment>
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <title>Your App Title</title> <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </React.Fragment>
+  );
 }
 
 export default MyApp;
