ca16dd983b6e36e5e0d556b5ec960b1403915b1f
Renato Olegário <73908159+renatoolegario@users.noreply.github.com>
2024-04-12 16:29:02 +0000

    uai

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,8 @@ import MapWithPolygon from './components/poligono';
 export default function Home() {
   return (
     <div>
-      <h1>Desenhar Polígono com LeafletJS</h1>
-      <MapWithPolygon />
+      <h1>Desenhar Polígono</h1>
+      
     </div>
   );
 }
