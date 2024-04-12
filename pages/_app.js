c9a03ccbc88e15b495e137070f3acab0ba6b3995
Renato Olegário <73908159+renatoolegario@users.noreply.github.com>
2024-04-12 18:08:26 +0000

    a

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,6 @@ function MyApp({ Component, pageProps }) {
   return (
     <React.Fragment>
       <Head>
-        <link rel="manifest" href="/manifest.json" />
         <title>Your App Title</title> <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <h1>Desenhar Polígono</h1>
-      
+      <MapWithPolygon />
     </div>
   );
 }
